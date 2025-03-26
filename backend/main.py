6a29b15b4f3319b10653874b27be119461d8476f
vhfantes-Dev <vhfantes.dev@gmail.com>
2025-03-26 20:09:30 -0300

    :zap: att. testando o a compressão do arquivo csv

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "scripts"))
 
 from downloads_pdf import baixar_pdfs 
+from extract_data import processar_pdf
 
 def main():
     print("🔍 Iniciando o download dos PDFs...")
@@ -12,6 +13,7 @@ def main():
         print("\n✅ Download concluído! Arquivos salvos em:")
         for arquivo in arquivos_baixados:
             print(f" - {arquivo}")
+        processar_pdf(arquivos_baixados)   
     else:
         print("\n⚠ Nenhum arquivo foi baixado.")
 
