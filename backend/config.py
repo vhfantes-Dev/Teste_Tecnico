4164f5952da2b4058bad8b6e93dcf131eed83037
vhfantes-Dev <vhfantes.dev@gmail.com>
2025-03-27 00:56:59 -0300

    :zap: att. passando a porta do xamp

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,7 @@
 DB_CONFIG = {
-    "host": "localhost",
+    "host": "localhost", 
+    "port":"3306",
     "user": "root",
-    "password": "senha",
+    "password": "",
     "database": "ans_dados"
 }
